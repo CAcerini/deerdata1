00d35411b3c5813be9ae8af30b4bcf5a93de79d2
CAcerini <acerinic@gmail.com>
2021-02-27 16:02:11 +0000

    simplifying & tidying

--- dataset1.R
+++ dataset1.R
@@ -17,12 +17,11 @@ View(Origional_dataset_1)
 dataset1 <- Origional_dataset_1
 
 #creating a new column of mass per pellet 
-dataset1 <- cbind(dataset1, ((dataset1$LastOfSampleMass)/(dataset1$LastOfPelletCount)) )
-names(dataset1)[names(dataset1)=="((dataset1$LastOfSampleMass)/(dataset1$LastOfPelletCount))"] <- "MassPerPellet"
+dataset1 <- dataset1 %>% mutate(MassPerPellet= LastOfSampleMass/LastOfPelletCount) %>% drop_na(MassPerPellet)
+
 
 #changing numbers to male and female
-dataset1$Sex <- ifelse(test=dataset1$Sex == 1, yes="Female", no="Male") 
-dataset1$Sex <- as.factor(dataset1$Sex)
+dataset1$Sex <- ifelse(test=dataset1$Sex == 1, yes="Female", no="Male") %>%  as.factor()
 
 #removing all NAs
 dataset1 <- dataset1 %>% drop_na()
@@ -34,7 +33,6 @@ dataset1 <- dataset1 %>% separate(LastOfSampleDate, sep = "-", into = c("SampYea
 
 dataset1$dearyear <- dataset1$`SampYear`
 dataset1$dearyear[dataset1$`SampMonth` == 4] <- dataset1$dearyear[dataset1$`SampMonth` == 4]-1
-dataset1$dearyear
 
 dataset1 <- dataset1 %>% separate(ExactDoD, sep = "-", into = c("DeathYear", "DeathMonth", "DeathDay")) %>% 
   mutate_at(c("DeathYear", "DeathMonth", "DeathDay"), as.numeric)
@@ -42,24 +40,15 @@ dataset1 <- dataset1 %>% separate(ExactDoD, sep = "-", into = c("DeathYear", "De
 dataset1$deathdearyear <- dataset1$`DeathYear`
 dataset1$deathdearyear[(dataset1$`DeathMonth` <5) & !is.na(dataset1$DeathMonth)] <- 
   dataset1$deathdearyear[(dataset1$`DeathMonth` <5) & !is.na(dataset1$DeathMonth)]-1
-dataset1$deathdearyear
 
 deathage1 <- (dataset1$deathdearyear-dataset1$BirthYear) %>% as.double()  
 dataset1$deathage <- deathage1
-dataset1$deathage %>% table()
-
 
 
 
 #create new dataset with just samples of November before death  
 Ndata1 <- dataset1 %>% filter(`SampMonth` == 11)
 
-#new dataset with november samples of juveniles <3 years 
-NdataJuv <- Ndata1 %>% filter( `death_age_years` < 3 )
-
-
-#new dataset with all month samples of juveniles <3 years 
-data1Juv <- dataset1 %>% filter( `death_age_years` < 3 )
 
 
 
@@ -67,7 +56,7 @@ data1Juv <- dataset1 %>% filter( `death_age_years` < 3 )
 
 
 
-#--------------------------------------------------------------------------
+#-------------------------------------------------------------------------- code graveyard
 
 
 #creating column with age
@@ -82,11 +71,15 @@ data1Juv <- dataset1 %>% filter( `death_age_years` < 3 )
 
 
 
-#create new column with samples months 
-#dataset1$samplemonth <- format(as.Date(dataset1$LastOfSampleDate), "%m")
-
-
 #creating new column with age categories, calf, 1-3 yrs and Adults
 #dataset1$age_group <- cut(dataset1$death_age_years,c(0,1,3,23))
 #levels(dataset1$age_group) = c("Calf","1-3 yrs","Adult")
 
+
+
+#new dataset with november samples of juveniles <3 years 
+#NdataJuv <- Ndata1 %>% filter( `deathage` < 3 )
+
+
+#new dataset with all month samples of juveniles <3 years 
+#data1Juv <- dataset1 %>% filter( `death_age_years` < 3 )
